6f4ff14bd0b2d1b8428cf961f30351e76fab7b8b
Ishaan Awasthi <ishaanawasthi05@gmail.com>
2025-11-05 13:39:59 -0800

    simple page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,22 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
+const ppNeueMontrealBold = localFont({
+  src: "../../public/OTF/PPNeueMontreal-Bold.otf",
+  variable: "--font-pp-neue-montreal-bold",
+  display: "swap",
+});
+
+const ppNeueMontrealThin = localFont({
+  src: "../../public/OTF/PPNeueMontreal-Thin.otf",
+  variable: "--font-pp-neue-montreal-thin",
+  display: "swap",
+});
+
+
 export const metadata: Metadata = {
-  title: "Site",
+  title: "ishaanawasthi! (.com)",
   description: "",
 };
 
@@ -12,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${ppNeueMontrealBold.variable} ${ppNeueMontrealThin.variable}`}>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
 export default function Home() {
-  return <main>Hello World</main>;
+  return<main className="relative">
+
+  <div className="flex flex-col items-center gap-0">
+
+    <div className="flex flex-col items-center">
+      <span className="font-(family-name:--font-pp-neue-montreal-bold) tracking-[-.1em] text-black text-[26rem] align-bottom select-none pointer-events-none">
+        ishaan!
+      </span>
+    </div>
+    
+    <div className="font-(family-name:--font-pp-neue-montreal-light) text-black text-4xl space-x-18 -mt-32 pointer-events-auto">
+      <a href="https://www.linkedin.com/in/ishaan-awasthi-2b541227b" target="_blank" rel="noopener noreferrer" className="underline">
+        linkedin
+      </a>
+
+      <a href="https://github.com/ishaan-awasthi" target="_blank" rel="noopener noreferrer" className="underline">
+        github
+      </a>
+
+      <a href="mailto:ishaanawasthi05@gmail.com" target="_blank" rel="noopener noreferrer" className="underline">
+        email
+      </a>
+
+      <a href="https://x.com/veryishaan" target="_blank" rel="noopener noreferrer" className="underline">
+        twitter
+      </a>
+
+      <a href="https://www.instagram.com/veryishaan/" target="_blank" rel="noopener noreferrer" className="underline">
+        instagram
+      </a>
+
+      <a href="https://ishaanawasthi.com" target="_blank" rel="noopener noreferrer" className="underline">
+        everything
+      </a>
+   
+    </div>
+  </div>
+
+</main>;
 }
