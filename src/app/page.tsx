bfbb854020c6ac6866e56dfd56f68046ae8f5937
Ishaan Awasthi <ishaanawasthi05@gmail.com>
2025-11-06 20:19:42 -0800

    Crossplatform optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,12 +20,12 @@ export default function Home() {
         style={{ zIndex: 0 }}
       >
         <div className="flex flex-col items-center -mt-16">
-          <span className="font-(family-name:--font-pp-neue-montreal-bold) tracking-[-.13em] text-black text-[26rem] select-none pointer-events-none">
+          <span className="font-(family-name:--font-pp-neue-montreal-bold) tracking-[-.13em] text-black text-[8rem] md:text-[16rem] lg:text-[26rem] select-none pointer-events-none">
             ishaan!
           </span>
         </div>
 
-        <div className="font-(family-name:--font-pp-neue-montreal-light) text-black text-4xl space-x-18 pointer-events-auto">
+        <div className="font-(family-name:--font-pp-neue-montreal-light) text-black text-xl md:text-2xl lg:text-4xl space-x-4 md:space-x-12 lg:space-x-18 pointer-events-auto">
           <a href="https://www.linkedin.com/in/ishaan-awasthi-2b541227b" target="_blank" rel="noopener noreferrer" className="hover:underline transition-all">
             linkedin
           </a>
